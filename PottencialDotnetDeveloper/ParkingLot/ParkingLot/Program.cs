6a2b7043a14aca3c03e888a2fd595cac2c4ecfbb
Matheus Gress <matheusaugustogress@gmail.com>
2024-03-22 01:10:49 -0300

    feat: add base code for the project

--- PottencialDotnetDeveloper/ParkingLot/ParkingLot/Program.cs
+++ PottencialDotnetDeveloper/ParkingLot/ParkingLot/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+    Console.WriteLine("Digite o preço incial:");
+    int.TryParse();
+
+    Console.WriteLine("Agora digite o preço por hora:");
+    int.TryParse();
+
+
+    Console.WriteLine("1 - Cadastrar veículo"); 
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+        bool Cadastro = true;
+
+     if (Cadastro) 
+     case 1;
+
+                
+     case 2;
+
+     case 3;
+   
+     case 4;
+
+        break;
+        Console.WriteLine("O programa se encerrou");
+    }
+
+
+
+    }  
+}
